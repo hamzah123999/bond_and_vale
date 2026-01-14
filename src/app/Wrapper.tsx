c12685b5de1845c47a44a8da5ed08a38d322d7b2
M Talha <talha18513@gmail.com>
2026-01-14 19:37:15 +0500

    remove client, reduce time

--- src/app/Wrapper.tsx
+++ src/app/Wrapper.tsx
@@ -2,10 +2,6 @@
 
 import { useEffect, useRef } from "react";
 import ClickSpark from "@/components/ClickSpark";
-import Lenis from "lenis";
-import "lenis/dist/lenis.css";
-import "aos/dist/aos.css";
-import AOS from "aos";
 import Footer from "@/components/Footer";
 
 interface Props {
@@ -13,23 +9,17 @@ interface Props {
     className?: string;
 }
 
-
-export default function Wrapper({ children, className = "" }: Props) {
-
+export default function Wrapper({ children }: Props) {
     const lenisRef = useRef<any>(null);
 
     useEffect(() => {
         let lenis: any;
 
         (async () => {
-            // Only load these in the browser when needed
             const [{ default: AOS }, { default: Lenis }] = await Promise.all([
                 import("aos"),
                 import("lenis"),
             ]);
-
-            await import("aos/dist/aos.css");
-            await import("lenis/dist/lenis.css");
 
             AOS.init({ once: true, duration: 800, delay: 100 });
 
@@ -39,34 +29,14 @@ export default function Wrapper({ children, className = "" }: Props) {
         })();
 
         return () => {
-            if (lenis) lenis.destroy?.();
+            lenis?.destroy?.();
         };
     }, []);
 
-    // useEffect(() => {
-    //     AOS.init({ once: true, duration: 800, delay: 100 });
-
-    //     if (window.innerWidth < 768) return; // disable on mobile
-
-    //     const lenis = new Lenis({ autoRaf: true });
-    //     lenisRef.current = lenis;
-
-    //     return () => lenis.destroy();
-    // }, []);
-
-
     return (
-        <>
-            <ClickSpark
-                sparkColor="#4a5a52"
-                sparkSize={10}
-                sparkRadius={15}
-                sparkCount={8}
-                duration={400}
-            >
-                {children}
-                <Footer />
-            </ClickSpark>
-        </>
-    )
+        <ClickSpark sparkColor="#4a5a52" sparkSize={10} sparkRadius={15} sparkCount={8} duration={400}>
+            {children}
+            <Footer />
+        </ClickSpark>
+    );
 }
